ef64f6da73569fb74a225b729290f6d1c348f850
anhkhoa1310 <hhuynhkhanhlinh2@gmail.com>
2025-04-19 21:23:44 +0700

    malicious_plugin.php

--- malicious_plugin.php
+++ malicious_plugin.php
@@ -1,5 +1,5 @@
 <?php
-file_put_contents(__DIR__ . "/hacked.txt", "Trang bị chèn mã lúc " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
+file_put_contents(__DIR__ . "/hacked.txt", "Trang bị chèn mã độc lúc " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
 
 // Hiển thị alert mỗi khi trang được load
 echo "<script>alert('Website đã bị chèn mã độc!');</script>";
